17c4bc430b3da334f3b6a5b8f1ab9473278eab0f
Rustem <rustemazimov1999@gmail.com>
2018-01-01 12:21:25 +0400

    Drag file action's provided

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -1,4 +1,52 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+
+import java.io.File;
+import java.util.List;
+
 public class HomePageController {
+
+    @FXML private TextField filePathLabel;
+
+    @FXML private Spinner<Integer> questionCountSpinner;
+
+    @FXML private Spinner<Integer> variantCountSpinner;
+
+    @FXML private TextField usernameTextField;
+
+    @FXML private PasswordField passwordField;
+
+    @FXML private void initialize() {
+
+    }
+
+    @FXML private void handleDragFileAction(DragEvent event) {
+        Dragboard db = event.getDragboard();
+        if (db.hasFiles())
+        {
+            List<File> files = db.getFiles();
+
+            if (files.size() != 1)
+            {
+                filePathLabel.setVisible(false);
+                filePathLabel.setText("");
+                return;
+            }
+
+            filePathLabel.setText(files.get(0).getAbsolutePath());
+        }
+        event.consume();
+    }
+
+    @FXML private void handleStartExamAction(){
+
+    }
+
 }
